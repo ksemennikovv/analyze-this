e819c6639bb4388fa64def8401ab41a8615ba4a0
ksemennikovv <ksemennikovv@gmail.com>
2026-05-10 19:16:19 +0300

    fix dev

--- api/auth-register.php
+++ api/auth-register.php
@@ -1,8 +1,6 @@
 <?php
-error_reporting(E_ALL);
+error_reporting(0);
 ini_set('display_errors', 0);
-ini_set('log_errors', 1);
-ini_set('error_log', __DIR__ . '/../logs/register_error.log');
 session_start();
 require_once __DIR__ . '/../config/database.php';
 require_once __DIR__ . '/../config/app.php';
